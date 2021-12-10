54385ce4f79fb4465f871d1484bb8d1fb18c413f
Sebastian Urrutia Torres <sebak93@gmail.com>
2021-12-10 02:02:25 -0300

    Agregar archivos de proyecto.

---
+++ Pronunciacion/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Pronunciacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine(File.Exists(@"..\..\..\data\1.txt"));
+            string palabra = "israel";
+            int contador = 0;
+            string[] arreglo = { "rr", "lr", "sr", "nr" };
+            List<string> lexemas = new List<string>(arreglo);
+            if (palabra.StartsWith("r") || palabra.StartsWith("R"))
+            {
+                contador = 1;
+            }
+            foreach (var lex in lexemas)
+            {
+                contador += Regex.Matches(palabra, lex, RegexOptions.IgnoreCase).Count;
+            }
+            Console.WriteLine(contador);
+        }
+    }
+}
---
+++ Pronunciacion/Texto.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Pronunciacion
+{
+    public class Texto
+    {
+        private string _titulo;
+        private int _numeroDePalabras;
+        List<string> textoEnLineas;
+        List<string> textoEnPalabras;
+        Dictionary<string, string> modosArticulatorios;
+
+        public Texto(string rutaTexto)
+        {
+            textoEnLineas = new List<string>();
+            textoEnLineas = File.ReadAllLines(rutaTexto).ToList();
+            _titulo = textoEnLineas[0];
+            textoEnPalabras = new List<string>();
+            SetTextoEnPalabras();
+            _numeroDePalabras = textoEnPalabras.Count();
+            modosArticulatorios = DeterminarModo(textoEnPalabras);
+        }
+
+        public string GetTitulo()
+        {
+            return _titulo;
+        }
+
+        private void SetTextoEnPalabras()
+        {
+            string[] delimitadores = { " ", ",", ".", ":", ";", "-", "—", "¿", "?", "¡", "!", "\t", "\r\n" };
+            foreach (var linea in textoEnLineas.Skip(1))
+            {
+                string[] palabrasEnLinea = linea.Split(delimitadores, System.StringSplitOptions.RemoveEmptyEntries);
+                textoEnPalabras.AddRange(palabrasEnLinea);
+            }
+        }
+
+        public List<string> GetTextoLineas()
+        {
+            return textoEnLineas;
+        }
+
+        public List<string> GetTextoPalabras()
+        {
+            return textoEnPalabras;
+        }
+
+        public int GetTextoNumeroPalabras()
+        {
+            return _numeroDePalabras;
+        }
+
+        // Se determina el modo de articulacion de cada palabra en el texto.
+        // Se utiliza diccionario para evitar dobles ocurrencias.
+        private Dictionary<string,string> DeterminarModo(List<string> palabras)
+        {
+            Dictionary<string, string> _modos = new Dictionary<string, string>();
+            foreach(var palabra in palabras)
+            {
+                if(palabra.Length <= 1)
+                {
+                    continue;
+                }
+                int rr = CountVibranteDoble(palabra);
+                int r = CountVibranteSimple(palabra);
+                int l = CountLaterales(palabra);
+                if(rr == r && rr == l && rr == 0)
+                {
+                    //No se puede clasificar
+                    continue;
+                }
+                if (rr >= r && rr>= l)
+                {
+                    AgregarModo(_modos,palabra,"VibranteDoble");
+                }
+                else if (r >= l)
+                {
+                    AgregarModo(_modos,palabra,"VibranteSimple");
+                }
+                else
+                {
+                    AgregarModo(_modos,palabra,"Lateral");
+                }
+            }
+            return _modos;
+        }
+
+        private int CountVibranteSimple(string palabra)
+        {
+            int contador = 0;
+            string[] arreglo = {"ar","er","ir","or","ur","br","cr","dr","fr","gr","pr","tr"};
+            List<string> lexemas = new List<string>(arreglo);
+            foreach(var lex in lexemas)
+            {
+                contador += Regex.Matches(palabra, lex, RegexOptions.IgnoreCase).Count;
+            }
+            return contador;
+        }
+
+        private int CountVibranteDoble(string palabra)
+        {
+            int contador = 0;
+            string[] arreglo = { "rr","lr","sr","nr"};
+            List<string> lexemas = new List<string>(arreglo);
+            if(palabra.StartsWith("r") || palabra.StartsWith("R"))
+            {
+                contador = 1;
+            }
+            foreach (var lex in lexemas)
+            {
+                contador += Regex.Matches(palabra, lex, RegexOptions.IgnoreCase).Count;
+            }
+            return contador;
+        }
+
+        private int CountLaterales(string palabra)
+        {
+            int contador = 0;
+            string[] arreglo = { "la", "le", "li", "lo", "lu", "al", "el", "il", 
+                "ol", "ul", "cl", "fl","gl", "pl", "tl" };
+            List<string> lexemas = new List<string>(arreglo);
+            if (palabra.StartsWith("l") || palabra.StartsWith("L"))
+            {
+                contador = 1;
+            }
+            foreach (var lex in lexemas)
+            {
+                contador += Regex.Matches(palabra, lex, RegexOptions.IgnoreCase).Count;
+            }
+            return contador;
+        }
+
+        private void AgregarModo(Dictionary<string,string> dic,string palabra, string modo)
+        {
+            try
+            {
+                dic.Add(palabra,modo);
+            }
+            catch
+            {
+                //no se puede debido a que ya esta
+            }
+        }
+
+    }
+}
